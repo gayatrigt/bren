32485afec422d5e903ead359137f7c9ed9f0e436
gayatrigt <taleygayatri@gmail.com>
2024-07-05 06:59:20 +0530

    added ackno

--- src/pages/api/newWebHook.ts
+++ src/pages/api/newWebHook.ts
@@ -63,6 +63,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
 
+    res.status(200).json({ message: 'Webhook received' });
+
     try {
         const webhookData = req.body as WebhookResponse;
 
