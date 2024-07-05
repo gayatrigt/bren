776461f4c9864cac1af7196216591c5971b92831
gayatrigt <taleygayatri@gmail.com>
2024-07-05 07:05:35 +0530

    error fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,7 @@ const config = {
     "@typescript-eslint/no-floating-promises": "warn",
     "@typescript-eslint/restrict-plus-operands": "warn",
     "@typescript-eslint/no-unsafe-return": "warn",
+    "@typescript-eslint/prefer-optional-chain": "warn",
     "@typescript-eslint/no-redundant-type-constituents": "warn",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
