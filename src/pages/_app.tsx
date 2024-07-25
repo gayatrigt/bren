077280bd7ba705369a612e89c9c78ec15e087548
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-07-25 21:50:34 +0530

    chore: Add Open Graph meta tags for improved social sharing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,19 @@ import "~/styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { RainbowWalletProvider } from "~/utils/rainbowConfig";
 import Layout from "~/components/layout";
+import Head from 'next/head';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <RainbowWalletProvider>
+      <Head>
+        <meta property="og:title" content="Bren" />
+        <meta property="og:site_name" content="Bren" />
+        <meta property="og:url" content="https://www.bren.lol" />
+        <meta property="og:description" content="Recognize your Based frens with $bren" />
+        <meta property="og:type" content="" />
+        <meta property="og:image" content="" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,12 @@
 import { Transaction } from "@prisma/client";
 import { NextPage } from "next";
-import Head from "next/head";
-import { useSearchParams } from "next/navigation";
 import Hero from "~/components/Hero";
 import SectionOne from "~/components/SectionOne";
 import SectionThree from "~/components/SectionThree";
 import SectionTwo from "~/components/SectionTwo";
-
 const Home: NextPage<{ transactions: Transaction[] }> = (props) => {
-  const queryParams = useSearchParams();
-  const tipStatus = encodeURIComponent(queryParams.get("tipStatus")!);
-  const statusMsg = encodeURIComponent(queryParams.get("msg")!);
-  const mainText = encodeURIComponent(queryParams.get("main")!);
-
   return (
     <>
-      <Head>
-        <title>Bren</title>
-        <meta name="description" content="Recognize your Based frens with $bren" />
-        <link rel="icon" href="/icon-bren.png" />
-        <meta property="fc:frame" content="vNext" />
-        <meta
-          property="og:image"
-          // content={`https://d4af-49-205-43-209.ngrok-free.app/api/getStatusImg/route?tipStatus=${tipStatus}&msg=${statusMsg}&main=${mainText}`}
-          content={``}
-        />
-        <meta
-          property="fc:frame:image"
-          content={`${process.env.NEXT_PUBLIC_BASE_URL}/api/getStatusImg/route?tipStatus=${tipStatus}&msg=${statusMsg}&main=${mainText}`}
-        />
-      </Head>
-
       <section className="w-full">
         <Hero />
         <SectionOne />
