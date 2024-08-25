fe209d8845be1a5f59f767a991d21935796a6fdc
gayatrigt <taleygayatri@gmail.com>
2024-08-25 20:28:24 +0530

    fix: increasing wit time

--- src/pages/api/telegramWebhook.ts
+++ src/pages/api/telegramWebhook.ts
@@ -160,7 +160,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             });
 
             // delay for 100ms
-            await new Promise(resolve => setTimeout(resolve, 500));
+            await new Promise(resolve => setTimeout(resolve, 1000));
 
             if (response.ok) {
                 console.log('Tip processed successfully.');
